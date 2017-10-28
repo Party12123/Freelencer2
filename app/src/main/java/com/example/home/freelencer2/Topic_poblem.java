e764145922612892cb57f93f06c3e855420e3f57
Party12123 <samrerng.kl.58@ubu.ac.th>
2017-10-28 21:23:19 +0700

    Hello2

---
+++ app/src/main/java/com/example/home/freelencer2/Topic_poblem.java
@@ -0,0 +1,13 @@
+package com.example.home.freelencer2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Topic_poblem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_poblem);
+    }
+}
