69f40265e7023319f29240e65c6d2bcdf97ab039
Party12123 <samrerng.kl.58@ubu.ac.th>
2017-11-07 15:34:14 +0700

    Select_technician

--- app/src/main/java/com/example/home/freelencer2/Detail_problem.java
+++ app/src/main/java/com/example/home/freelencer2/Detail_problem.java
@@ -1,7 +1,9 @@
 package com.example.home.freelencer2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Context;
@@ -9,7 +11,7 @@ import android.support.v7.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
-public class Detail_problem extends AppCompatActivity {
+public class Detail_problem extends AppCompatActivity implements View.OnClickListener {
     private Button next;
     private EditText detail_Name;
     private EditText detail_Phone;
@@ -24,6 +26,14 @@ public class Detail_problem extends AppCompatActivity {
         detail_Address = findViewById(R.id.editTextAddress);
         detail_Phone = findViewById(R.id.editTextPhone);
         detail_Problem = findViewById(R.id.editTextProblem);
+        next = findViewById(R.id.nextButton);
+        next.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view == next){
+            startActivity(new Intent(Detail_problem.this,Select_Technician.class));
+        }
+    }
 }
--- app/src/main/java/com/example/home/freelencer2/Select_Technician.java
+++ app/src/main/java/com/example/home/freelencer2/Select_Technician.java
@@ -2,12 +2,47 @@ package com.example.home.freelencer2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class Select_Technician extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private RecyclerView.Adapter<technicianViewholder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select_technician);
+        recyclerView = new RecyclerView(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerView.Adapter<technicianViewholder>(){
+
+            @Override
+            public technicianViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View list_Technician = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.cardview_technician,parent,false);
+                return new technicianViewholder(list_Technician);
+            }
+
+            @Override
+            public void onBindViewHolder(technicianViewholder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 1;
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        setContentView(recyclerView);
+    }
+    private class technicianViewholder extends RecyclerView.ViewHolder{
+
+        public technicianViewholder(View itemView) {
+            super(itemView);
+        }
     }
 }
